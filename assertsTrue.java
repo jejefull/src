e88502403a86cdbb3d3c707f58a6be8543906fd2
jejefull <68730656+jejefull@users.noreply.github.com>
2021-02-24 20:46:42 +0200

    Create assertsTrue.java

---
+++ assertsTrue.java
@@ -0,0 +1,12 @@
+
+package com.company;
+import static org.junit.Assert.assertTrue;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! jeje");
+        assertTrue(true);
+
+    }
+}
