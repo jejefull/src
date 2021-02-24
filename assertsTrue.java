2c8f7b3669342b660cafb4226108a3840ffda39d
jejefull <68730656+jejefull@users.noreply.github.com>
2021-02-24 20:59:04 +0200

    Update assertsTrue.java

--- assertsTrue.java
+++ assertsTrue.java
@@ -3,7 +3,7 @@ package com.company;
 import static org.junit.Assert.assertTrue;
 
 
-public class Main {
+public class assertsTrue {
     public static void main(String[] args) {
         System.out.println("Hello world! jeje");
         assertTrue(true);
